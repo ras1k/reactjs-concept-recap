2cca124aee1987fcfe934717c126e4e0a5cf414f
ras1k <rasikabdullah@gmail.com>
2023-04-21 01:53:38 +0600

    state added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -10,11 +11,27 @@ function App() {
   );
 }
 
+const lol = {
+  margin: '20px',
+  backgroundColor: 'lightblue',
+  padding: '10px',
+  borderRadius: '10px'
+}
+
 function District (props){
+  const[power, setPower] = useState(10);
+
+  const boostPower = () =>{
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+
   return (
-    <div className='disrict'>
+    <div className='disrict' style={lol}>
       <h2>Name : {props.name}</h2>
       <p>Speciality : {props.speciality}</p>
+      <h4>Power : {power}</h4>
+      <button onClick={boostPower}>Boost Power</button>
     </div>
   )
 }
