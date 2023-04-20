4b89068e254cea1c8d943cd913e07c590f726a7e
ras1k <rasikabdullah@gmail.com>
2023-04-21 02:21:11 +0600

    api  added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name="Rasik" speciality='Lol'></District>
       <District name="Tazim" speciality='Lol'></District>
     </div>
   );
+}
+
+function LoadPosts(){
+  const [posts, setPosts] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=> res.json())
+    .then(data => setPosts(data))
+  },[]);
+  return(
+    <div>
+      <h1>lol : {posts.length}</h1>
+      {
+        posts.map(post => <Post title={post.title} body={post.body} ></Post>)
+      }
+    </div>
+  )
+}
+
+function Post (props){
+  return(
+    <div style={{
+      backgroundColor : 'cyan',
+      padding : '10px',
+      borderRadius: '8px',
+      margin: '15px'
+    }}>
+      <h2>Title : {props.title}</h2>
+      <p>Body : {props.body}</p>
+    </div>
+  )
 }
 
 const lol = {
